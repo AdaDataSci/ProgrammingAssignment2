7444ef26b79500ad55b71b11aaff97eb2749183d
AdaDataSci <adannad02@yahoo.com>
2017-05-22 14:18:35 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Get a matrix, check to see if matrix is already cached, calculate inverse of matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+    m <- NULL                     
+    set <- function(y) {                      
+    x <<- y
+    m <<- NULL              
+    }
+    get <- function() x                           
+    setinverse <- function(solve) m <<- solve 
+    getinverse <- function() m        
+    list(set = set, get = get,                    
+    setinverse = setinverse,
+    getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## checks to see if inverse of matrix already exists
+## calculates inverse if it does not already exist
+##retrieve if it exists
+cacheSolve<- function(x, ...) {                 
+    m <- x$getinverse()
+    if(!is.null(m)) {                 
+    message("getting Inverse of the matrix")
+    return(m)
+    }
+    matrix <- x$get()                               
+    m <- solve(matrix, ...)
+    x$setinverse(m)
+    m
 }
